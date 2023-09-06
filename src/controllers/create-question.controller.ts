9c8e3251425fb15ddc27c084b0cc77942ea2dba5
Guilherme Spati <guilherme.spati723@gmail.com>
2023-09-05 23:15:19 -0300

    finished basics with e2e tests

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { randomUUID } from "crypto";
+import { CurrentUser } from "@/auth/current-user-decorator";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { UserPayload } from "@/auth/jwt.strategy";
+import { ZodValidationPipe } from "@/pipes/zod-validation.pipe";
+import { PrismaService } from "@/prisma/prisma.service";
+import { z } from "zod";
+
+const createQuestionBodySchema = z.object({
+    title: z.string(),
+    content: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+    constructor(
+        private prisma: PrismaService,
+    ) {}
+
+    @Post()
+    async handle(
+        @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+        @CurrentUser() user: UserPayload,
+    ) {
+        const { title, content } = body;
+        const userId = user.sub;
+
+        await this.prisma.question.create({
+            data: {
+                authorId: userId,
+                title,
+                content,
+                slug: randomUUID()
+            }
+        });
+    }
+}
