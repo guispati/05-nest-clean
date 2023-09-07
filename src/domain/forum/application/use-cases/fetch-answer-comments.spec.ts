93f500df3b2b93ad711ea8a3238b53b457486817
Guilherme Spati <guilherme.spati723@gmail.com>
2023-09-06 21:23:12 -0300

    integrate with domain layer

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -0,0 +1,51 @@
+import { makeAnswer } from 'test/factories/make-answer';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository';
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments';
+import { makeAnswerComment } from 'test/factories/make-answer-comment';
+
+let inMemoryAnswersCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: FetchAnswerCommentsUseCase;
+
+describe('Fetch Answer Answers', () => {
+    beforeEach(() => {
+        inMemoryAnswersCommentsRepository = new InMemoryAnswerCommentsRepository();
+        sut = new FetchAnswerCommentsUseCase(inMemoryAnswersCommentsRepository);
+    });
+
+    it('should be able to fetch answer comments ', async () => {
+        await inMemoryAnswersCommentsRepository.create(makeAnswerComment({
+            answerId: new UniqueEntityID('answer-1'),
+        }));
+
+        await inMemoryAnswersCommentsRepository.create(makeAnswerComment({
+            answerId: new UniqueEntityID('answer-1'),
+        }));
+
+        await inMemoryAnswersCommentsRepository.create(makeAnswerComment({
+            answerId: new UniqueEntityID('answer-1'),
+        }));
+
+        const result = await sut.execute({
+            answerId: 'answer-1',
+            page: 1,
+        });
+
+        expect(result.value?.answerComments).toHaveLength(3);
+    });
+
+    it('should be able to fetch paginated answer comments', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await inMemoryAnswersCommentsRepository.create(makeAnswerComment({
+                answerId: new UniqueEntityID('answer-1'),
+            }));
+        }
+
+        const result = await sut.execute({
+            answerId: 'answer-1',
+            page: 2,
+        });
+
+        expect(result.value?.answerComments).toHaveLength(2);
+    });
+});
